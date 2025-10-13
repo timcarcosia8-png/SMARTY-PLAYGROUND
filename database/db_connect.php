ae2bd8a502c293284693587dc1877392faa367e9
justinexocampo <ocampo.justine.f.2199@gmail.com>
2025-10-13 17:16:09 +0800

    user_game.php

--- database/db_connect.php
+++ database/db_connect.php
@@ -1,11 +1,12 @@
 <?php
-session_start();
-$bdservername = "localhost";
-$bdusername = "root";
-$bdpassword = "";
+if (session_status() == PHP_SESSION_NONE) session_start();
+
+$dbservername = "localhost";
+$dbusername = "root";
+$dbpassword = "";
 $database = "smarty_playground";
 
-$conn = mysqli_connect($bdservername, $bdusername, $bdpassword, $database);
+$conn = mysqli_connect($dbservername, $dbusername, $dbpassword, $database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
